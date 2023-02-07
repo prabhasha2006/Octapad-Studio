5a4b06ce243021cbde58b39f225d368d9690b56d
XCODEJET <102839813+prabhasha2006@users.noreply.github.com>
2023-02-07 14:14:37 +0530

    OctaPad Studio 2023

---
+++ homepage.js
@@ -0,0 +1,14 @@
+const getStart = () => {
+    document.getElementsByClassName("octapad-home-body")[0].style.display = "none"
+    document.getElementsByClassName("octapad-home-body")[1].style.display = "flex"
+    document.getElementById("loading-video").play()
+    setTimeout(openApp, 5300)
+}
+const openApp = () => {
+    window.open("https://htmlpreview.github.io/?https://github.com/prabhasha2006/Octapad-studio/blob/main/octapad.html", "_self")
+}
+
+//* Click & open without href=""
+const window_open = (url,type) => {
+    window.open(url,type)
+}
